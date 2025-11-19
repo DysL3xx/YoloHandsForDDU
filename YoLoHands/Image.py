0932709b404d45bc0c3325fc0baedfc12a006f47
Dex <jeppeandersen@outlook.dk>
2025-11-19 11:50:24 +0100

    start

---
+++ YoLoHands/Image.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+
+model = YOLO("runs/detect/train/weights/best.pt")
+results = model("datasets/valid/images/0027_jpg.rf.80534496b002b67075d072e5c474948a.jpg")
+results[0].show()
+
